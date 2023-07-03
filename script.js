f9b15c0bbaa1c65c5564b394c11706cc2a07e5b2
Delecroix <d-alexis@hotmail.fr>
2023-07-03 13:13:12 +0200

    w3c

--- script.js
+++ script.js
@@ -1,75 +1,140 @@
-
-// let john = document.getElementById("johnBlack");
-// let cardJohn = document.getElementById("cardJohn")
-
-
-
-// john.addEventListener("click", function() {
-//     console.log("coucou");
-//     cardJohn.classList.toggle("active")
-//     john.setAttribute("src", "./images/johnsonblack.png");
-
-
-// })
-
-
-// let btnJohn = document.getElementById("btnJohn");
-// let imgBlack = document.getElementById("imgBlack");
-// let imgColor = document.getElementById("imgColor");
-// let card = document.getElementById("card");
-
-// function accordeon() {
-
-//     imgBlack.classList.toggle("activeJohn");
-//     imgColor.classList.toggle("activeJohn")
-//     card.classList.toggle("activeJohn");
-// }
-
-
+// Récupération des id (image)
 let img = document.getElementById("img");
 let img1 = document.getElementById("img1");
-let main = document.getElementById("main");
+let img2 = document.getElementById("img2");
+let img3 = document.getElementById("img3");
+let img4 = document.getElementById("img4");
 
+// Récupération des btn
+let btn = document.getElementById("btn");
+let btn1 = document.getElementById("btn1");
+let btn2 = document.getElementById("btn2");
+let btn3 = document.getElementById("btn3");
+let btn4 = document.getElementById("btn4");
 
-let etat = true;
-function accordeonJohn() {
- etat = !etat;
-     if  (etat) {
-        img.querySelector(".imgBlack").style.left = "250px";
-        img.querySelector(".imgColor").style.bottom = "500px";
-        img.querySelector(".imgColor").style.width = "140px";
+// Function qui ajoute les classes actives.
+function myfunction(el) {
+  // Si l'élément id est strictement égal à test
+  if (el.getAttribute("id") === "img" || el.getAttribute("id") === "btn") {
+    img.classList.toggle("active");
+    img.querySelector(".imgColor").classList.toggle("active");
+    // Suppression
+    img1.classList.remove("active");
+    img1.querySelector(".imgColor").classList.remove("active");
+    img2.classList.remove("active");
+    img2.querySelector(".imgColor").classList.remove("active");
+    img3.classList.remove("active");
+    img3.querySelector(".imgColor").classList.remove("active");
+    img4.classList.remove("active");
+    img4.querySelector(".imgColor").classList.remove("active");
+  } else if (
+    el.getAttribute("id") === "img1" ||
+    el.getAttribute("id") === "btn1"
+  ) {
+    img1.classList.toggle("active");
+    img1.querySelector(".imgColor").classList.toggle("active");
 
-        img.querySelector(".card").style.right = "560px";
-     } else {
-        img.querySelector(".imgBlack").style.removeProperty("left");
-        img.querySelector(".imgColor").style.removeProperty("bottom");
-        img.querySelector(".imgColor").style.removeProperty("width");
+    // Suppression
+    img.classList.remove("active");
+    img.querySelector(".imgColor").classList.remove("active");
+    img2.classList.remove("active");
+    img2.querySelector(".imgColor").classList.remove("active");
+    img3.classList.remove("active");
+    img3.querySelector(".imgColor").classList.remove("active");
+    img4.classList.remove("active");
+    img4.querySelector(".imgColor").classList.remove("active");
+  } else if (
+    el.getAttribute("id") === "img2" ||
+    el.getAttribute("id") === "btn2"
+  ) {
+    img2.classList.toggle("active");
+    img2.querySelector(".imgColor").classList.toggle("active");
 
-        img.querySelector(".card").style.removeProperty("right");
-     }
+    // Suppression
+    img.classList.remove("active");
+    img.querySelector(".imgColor").classList.remove("active");
+    img1.classList.remove("active");
+    img1.querySelector(".imgColor").classList.remove("active");
+    img3.classList.remove("active");
+    img3.querySelector(".imgColor").classList.remove("active");
+    img4.classList.remove("active");
+    img4.querySelector(".imgColor").classList.remove("active");
+  } else if (
+    el.getAttribute("id") === "img3" ||
+    el.getAttribute("id") === "btn3"
+  ) {
+    img3.classList.toggle("active");
+    img3.querySelector(".imgColor").classList.toggle("active");
+
+    // Suppression
+    img.classList.remove("active");
+    img.querySelector(".imgColor").classList.remove("active");
+    img1.classList.remove("active");
+    img1.querySelector(".imgColor").classList.remove("active");
+    img2.classList.remove("active");
+    img2.querySelector(".imgColor").classList.remove("active");
+    img4.classList.remove("active");
+    img4.querySelector(".imgColor").classList.remove("active");
+  } else if (
+    el.getAttribute("id") === "img4" ||
+    el.getAttribute("id") === "btn4"
+  ) {
+    img4.classList.toggle("active");
+    img4.querySelector(".imgColor").classList.toggle("active");
+
+    // Suppression
+    img.classList.remove("active");
+    img.querySelector(".imgColor").classList.remove("active");
+    img1.classList.remove("active");
+    img1.querySelector(".imgColor").classList.remove("active");
+    img2.classList.remove("active");
+    img2.querySelector(".imgColor").classList.remove("active");
+    img3.classList.remove("active");
+    img3.querySelector(".imgColor").classList.remove("active");
+  }
 }
 
+img.addEventListener("click", () => {
+  // console.log(img);
+  // console.log(img.getAttribute("id"));
+  // L'élément en paramètre
+  myfunction(img);
+});
 
-function accordeonVin() {
-    etat = !etat;
-    if (etat) {
-        img1.querySelector(".imgBlack").style.left = "250px";
-        img1.querySelector(".imgColor").style.bottom = "500px";
-        img1.querySelector(".imgColor").style.width = "280px";
-        img1.querySelector(".imgColor").style.zIndex = "5";
+btn.addEventListener("click", () => {
+  myfunction(img);
+});
 
-        img1.querySelector(".card").style.right = "418px";
-    } else {
-        img1.querySelector(".imgBlack").style.removeProperty("left");
-        img1.querySelector(".imgColor").style.removeProperty("bottom");
-        img1.querySelector(".imgColor").style.removeProperty("width");
-        img1.querySelector(".imgColor").style.removeProperty("z-index");
+img1.addEventListener("click", () => {
+  myfunction(img1);
+});
 
-        img1.querySelector(".card").style.removeProperty("right");
-    }
-}
+btn1.addEventListener("click", () => {
+  myfunction(img1);
+});
 
+img2.addEventListener("click", () => {
+  myfunction(img2);
+});
 
+btn2.addEventListener("click", () => {
+  myfunction(img2);
+});
 
+img3.addEventListener("click", () => {
+  myfunction(img3);
+});
+
+btn3.addEventListener("click", () => {
+  myfunction(img3);
+});
+
+img4.addEventListener("click", () => {
+  myfunction(img4);
+});
+
+btn4.addEventListener("click", () => {
+  myfunction(img4);
+});
 
 
